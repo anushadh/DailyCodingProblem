9244b2543430cec92ee96db630444ec91b84a624
Anusha <anushadhulipudi.ad@gmail.com>
2018-06-01 08:39:10 +0530

    #Day2
    Day 2 of coding.
    Array structures

--- CodingProblems/src/problems/Day2.java
+++ CodingProblems/src/problems/Day2.java
@@ -1,5 +1,7 @@
 package problems;
 
+import java.util.Arrays;
+
 /*This problem was asked by Uber.
 
 Given an array of integers, return a new array such that each element at index i of the new array 
@@ -12,6 +14,44 @@ Follow-up: what if you can't use division?
 */
 public class Day2 {
 	
-	public static void 
+	public static void main(String args[]) {
+		int[] array = new int[]{1, 2, 3, 4, 5};
+		//int[] array = new int[]{3, 2, 1};
+		int size = array.length;
+		int[] newArray = new int[size];
+		findNewArray(array, size, newArray);
+		findNewArray(array,  newArray, size);
+	}
+	
+	//Approach 1: Using product and division
+	static void findNewArray(int[] array, int size, int[] newArray) {
+		int product = 1;
+		for(int i = 0; i < size; i++) {
+			product *= array[i];
+		}
+		System.out.println("Product of array:: " + product);
+		
+		for(int i = 0; i < size; i++) {
+			newArray[i] = product/array[i];
+		}
+		System.out.println(Arrays.toString(newArray));
+	}
+	
+	//Approach 2: without using division
+	static void findNewArray(int[] array, int[] newArray, int size) {
+		for(int i = 0; i < size; i++) {
+			int product = 1;
+			for(int j = 0; j < size; j++) {
+				if(i != j) {
+					product *= array[j];
+				}
+			}
+			newArray[i] = product;
+		}
+		
+		System.out.println(Arrays.toString(newArray));
+	}
+	
+	
 
 }
